9d31fc8d5c61aab246655aa566ed134970474438
Tyler Koch <ttkochdev@gmail.com>
2019-08-01 16:23:16 -0500

    first commit

---
+++ src/networksecurity_ssl_lab/SSLClient.java
@@ -0,0 +1,24 @@
+package networksecurity_ssl_lab;
+
+/**
+ *
+ * @author ttkoch
+ */
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.*;
+
+public class SSLClient {
+
+    public static void main(String[] args) throws IOException {
+        SSLSocketFactory ssf =
+                (SSLSocketFactory) SSLSocketFactory.getDefault();
+        Socket sslSocket = ssf.createSocket("localhost", 50000);
+
+        PrintWriter pR = new PrintWriter(sslSocket.getOutputStream(), true);
+        pR.println("Hello Secure Sockets!");
+        pR.println("I Love security!");
+        pR.close();
+        sslSocket.close();
+    }
+}
